398d245b51e830690937a7295a67541fccb01f14
StellaEXE <ikpeterson18@gmail.com>
2025-01-17 14:17:21 -0500

    Add files via upload

---
+++ My first Python Program.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("I just wrote my first Python Program")
+print("This is so much fun")
+name = input("Hello, what is your name?")  #name
+print("Welcome, it is so nice to meet you " + name) #name
+
+STRING_CATS = "cats"
+STRING_DOGS = "dogs"
+finalStatement = "It is raining " + STRING_CATS + " and " + STRING_DOGS
+print(finalStatement)
